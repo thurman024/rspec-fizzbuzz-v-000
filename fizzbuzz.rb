10f80297d8d09bfe1e6a89b7b50d8915c776bd7b
Stephen Thurman <thurman024@gmail.com>
2018-01-16 13:56:56 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@ def fizzbuzz(n)
   r_from3 = n % 3
   r_from5 = n % 5
   if r_from3 == 0 && r_from5 == 0
-    return "fizzbuzz"
+    "fizzbuzz"
   elsif r_from3 == 0
-    return "fizz"
+    "fizz"
   elsif r_from5 == 0
-    return "buzz"
+    "buzz"
   else
-    return nil
+    nil
   end
 end
